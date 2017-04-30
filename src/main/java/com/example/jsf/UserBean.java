208277d8dd898ef2ee148cac4c3c2ea956f0c796
krvemula <vemula.radhika7@gmail.com>
2017-04-30 01:09:24 -0500

    Add files via upload

---
+++ src/main/java/com/example/jsf/UserBean.java
@@ -0,0 +1,32 @@
+package com.example.jsf;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+ 
+@ManagedBean(name="user")
+@SessionScoped
+public class UserBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	int id;
+	String email;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
